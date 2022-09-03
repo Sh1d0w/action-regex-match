291faf94a995947ed070f70b202c94221f75e3a6
Sh1d0w <5074917+Sh1d0w@users.noreply.github.com>
2022-09-03 10:33:15 +0300

    build

--- dist/index.js
+++ dist/index.js
@@ -44,19 +44,15 @@ function run() {
             const regex = core.getInput('regex');
             const flags = core.getInput('flags');
             const re = new RegExp(regex, flags);
-            const result = re.exec(text);
-            if (result) {
-                for (const [index, x] of result.entries()) {
-                    if (index === 10) {
-                        return;
-                    }
-                    if (index === 0) {
-                        core.setOutput('match', x);
-                        continue;
-                    }
-                    core.setOutput(`group${index}`, x);
+            let m;
+            const result = [];
+            do {
+                m = re.exec(text);
+                if (m) {
+                    result.push(m[0]);
                 }
-            }
+            } while (m);
+            core.setOutput('issues', result.join(','));
         }
         catch (error) {
             core.error(error);
